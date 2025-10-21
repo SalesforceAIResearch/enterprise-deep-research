9a00a8d9a57536d29d5aab9bc1b0c9e62d6d67f4
Akshara Prabhakar <akshara.prabhakar@salesforce.com>
2025-10-21 16:17:34 -0700

    benchmark scripts release

--- benchmarks/run_research.sh
+++ benchmarks/run_research.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Echo script start information
 echo "🚀 Starting run_research.sh script..."
 echo "📅 Start time: $(date)"
-echo "🔢 Script PID: $$"
 echo "👤 User: $(whoami)"
 echo "📁 Working directory: $(pwd)"
 echo "==============================================="
@@ -11,14 +9,22 @@ echo "==============================================="
 LOGS_DIR="logs"
 mkdir -p $LOGS_DIR
 
+## Simple test
+python -u run_research.py "what is ai?" \
+  --max-loops 2 \
+  --output sample_result.json > $LOGS_DIR/traj.log 2>&1 &
 
-# python -u run_research_concurrent.py --benchmark-type rqa --rate-limit 1.0 --max-workers 10 --provider google --model gemini-2.5-pro > $LOGS_DIR/rqa_test.log 2>&1 &
+## DeepResearch Bench (DRB)
+# python -u run_research_concurrent.py \
+# --benchmark drb \
+# --input /Users/akshara.prabhakar/Documents/deep_research/benchmarks/deep_research_bench/data/prompt_data/query.jsonl \
+# --output_dir drb_trajectories \
+# --collect-traj \
+# --task_ids 81 > $LOGS_DIR/drb_traj1.log 2>&1 &
 
-python -u run_research_concurrent.py --benchmark-type deepconsult --rate-limit 1.0 --max-workers 6 --provider google --model gemini-2.5-pro > $LOGS_DIR/deepconsult_test_10_loops.log 2>&1 &
-
-## CONCURRENT
-# python -u run_research_concurrent.py --max-workers 2 --max-loops 10  --rate-limit 3 --disable-visualizations --task-ids '1,2,3,51,52' > $LOGS_DIR/sfr_auto_report.log 2>&1 &
-
-### ResearchQA
-## SEQUENTIAL
-# python run_research.py "What are the key factors affecting the compressive strength and hardening behavior of geopolymer concretes that utilize Shirasu aggregates?" --max-loops 2 --output /Users/akshara.prabhakar/Documents/deep_research/benchmarks/gaia_results/sfr_test_report.json --disable-visualizations --benchmark-mode
+## DeepConsult
+# python -u run_research_concurrent.py \
+# --benchmark deepconsult \
+# --input ydc-deep-research-evals/datasets/DeepConsult/queries.csv \
+# --limit 1 \
+# --output_dir deepconsult_trajectories > $LOGS_DIR/deepconsult_traj.log 2>&1 &
